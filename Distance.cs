a0cc770118771daac8ca567fd3a397629d29383a
Arjun Nair <nair.arjun47@yahoo.com>
2021-02-12 21:02:44 -0800

    Test Connection
    Test connection between two classes to check if they work. It works.

---
+++ Distance.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BGCBackend {
+    
+    public class Distance {
+
+        public void printOut(String output) {
+            Console.WriteLine(output);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace BGCBackend
 {
@@ -6,6 +9,10 @@ namespace BGCBackend
     {
         static void Main(string[] args)
         {
+
+            Distance constructorClass = new Distance();
+            constructorClass.printOut("Connected!");
+
             Console.WriteLine("Hello World!");
         }
     }
